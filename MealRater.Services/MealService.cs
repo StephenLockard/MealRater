d8b6d3562a1ffe08d192d3a9fb9061e1e1069ef4
Stephen <>
2019-02-08 09:59:30 -0500

    Create and details service/model/controller complete, adding views and update/delete functions next

--- MealRater.Services/MealService.cs
+++ MealRater.Services/MealService.cs
@@ -10,7 +10,8 @@ namespace MealRater.Services
 {
     public class MealService
     {
-        public bool CreateNote(MealCreate model)
+
+        public bool CreateMeal(MealCreate model)
         {
             var entity =
                 new Meal()
@@ -28,26 +29,45 @@ namespace MealRater.Services
             }
         }
 
-            public IEnumerable<MealListItem> GetMeals()
+        public IEnumerable<MealListItem> GetMeals()
+        {
+            using (var ctx = new ApplicationDbContext())
             {
-                using (var ctx = new ApplicationDbContext())
-                {
-                    var query =
-                       ctx
-                         .Meals
-                         .Select(
-                             e =>
-                                 new MealListItem
-                                 {
-                                     MealID = e.MealId,
-                                     MealName = e.MealName,
-                                     MealDescription = e.MealDescription,
-                                     CreatedUtc = e.CreatedUtc
-                                 }
-                         );
-
+                var query =
+                   ctx
+                     .Meals
+                     .Select(
+                         e =>
+                             new MealListItem
+                             {
+                                 MealID = e.MealId,
+                                 MealName = e.MealName,
+                                 MealDescription = e.MealDescription,
+                                 CreatedUtc = e.CreatedUtc
+                             }
+                     );
                 return query.ToArray();
-                }
             }
         }
+
+        public MealDetail GetMealById(int mealId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Meals
+                        .Single(e => e.MealId == mealId);
+                return
+                    new MealDetail
+                    {
+                        MealId = entity.MealId,
+                        MealName = entity.MealName,
+                        MealDescription = entity.MealDescription,
+                        CreatedUtc = entity.CreatedUtc
+                    };
+            }
+
+        }
     }
+}
