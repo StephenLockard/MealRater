2f99b0894b0fc734910fbc127c69f8e2be72d058
StephenLockard <44308629+Trancefate@users.noreply.github.com>
2019-02-11 10:23:01 -0500

    Table relationships complete, Vote list item and view created.

---
+++ MealRater.Models/VoteListItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealRater.Models
+{
+    public class VoteListItem
+    {
+        public int VoteID { get; set; }
+        public int MealID { get; set; }
+        public string MealName { get; set; }
+        [Display(Name = "Vote Date"), DisplayFormat(DataFormatString = "{0:d}")]
+        public DateTimeOffset CreatedUtc { get; set; }
+        public string UserName { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ MealRater.WebMVC/Controllers/MealVoteController.cs
@@ -0,0 +1,22 @@
+﻿using MealRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MealRater.WebMVC.Controllers
+{
+    [Authorize]
+    public class MealVoteController : Controller
+    {
+        // GET: MealVote
+        public ActionResult Index()
+        {
+            var model = new VoteListItem[0];
+            return View(model);
+        }
+
+        
+    }
+}
